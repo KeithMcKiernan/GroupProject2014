ae09a482dc974943d72b9f4c9fee09eee5f9276b
KeithMcKiernan <sales@jdmtrendz.com>
2014-02-17 20:11:25 +0000

    Add footer.php.

---
+++ footer.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * The footer of the website
+ * Displays all of the footer information and closes the </body> and </html> tags
+ *
+ * @package College Social Media Website
+ * @since 	Version 1.0
+ *
+ */
+
+?>
+		<!-- </div> -->
+	</body>
+</html>
