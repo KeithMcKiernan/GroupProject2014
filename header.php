ffdd1974cab144a45aa955f21378c13192f49797
KeithMcKiernan <sales@jdmtrendz.com>
2014-02-17 20:17:24 +0000

    Add header.php.

---
+++ header.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * The header file of the website
+ * Displays all of the <head> section and all information as far as <div id="container">
+ *
+ * @package College Social Media Website
+ * @since 	Version 1.0
+ *
+ */
+
+?>
+
+<!DOCTYPE html>
+
+	<!-- [if lt IE 7]> 			<html class="no-js lt-ie9 lt-ie8 lt-ie7"> 	<![endif]-->
+	<!-- [if IE 7]> 				<html class="no-js lt-ie9 lt-ie8"> 			<![endif]-->
+	<!-- [if IE 8]> 				<html class="no-js lt-ie9"> 				<![endif]-->
+	<!-- [if gt IE 8]> -->		<html class="no-js" > 						<!-- <![endif]-->
+
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+
+		<title>Register</title>
+
+		<!-- Link to Stylesheets -->
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+		
+		<!-- JQuery Code, may load this below in the footer for better performance -->
+		<script src="../group2/public/js/jquery-1.11.0.min.js"></script>
+		<script src="../group2/public/js/script.js"></script>
+	</head>
+ 
+	<body>
+		<!-- <div id="container"> -->
