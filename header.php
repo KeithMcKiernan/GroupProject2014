14b27f075bc2acfbc2d0ef6a8752b3c660d7e0fa
Keith McKiernan <keito-1@hotmail.com>
2014-02-17 20:56:55 +0000

    Update header.php

--- header.php
+++ header.php
@@ -12,10 +12,11 @@
 ?>
 
 <!DOCTYPE html>
-	<!-- [if lt IE 7]> 			<html class="no-js lt-ie9 lt-ie8 lt-ie7"> 		<![endif]-->
-	<!-- [if IE 7]> 				<html class="no-js lt-ie9 lt-ie8"> 				<![endif]-->
-	<!-- [if IE 8]> 				<html class="no-js lt-ie9"> 					<![endif]-->
-	<!-- [if gt IE 8]> --> 		<html class="no-js" > 							<!-- <![endif]-->
+
+	<!-- [if lt IE 7]> 		<html class="no-js lt-ie9 lt-ie8 lt-ie7"> 	<![endif]-->
+	<!-- [if IE 7]> 		<html class="no-js lt-ie9 lt-ie8"> 			<![endif]-->
+	<!-- [if IE 8]> 		<html class="no-js lt-ie9"> 				<![endif]-->
+	<!-- [if gt IE 8]> --> 	<html class="no-js" > 						<!-- <![endif]-->
 
 	<head>
 		<meta charset="utf-8" />
@@ -36,4 +37,4 @@
 	</head>
  
 	<body>
-		<!-- <div id="container"> -->
+	<!-- <div id="container"> -->
