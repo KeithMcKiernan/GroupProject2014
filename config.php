c7f365854d467f9bd18725f994585b69b921de11
KeithMcKiernan <sales@jdmtrendz.com>
2014-02-17 20:10:46 +0000

    Add config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+	/**
+	 *	Configuration: Database Connection 
+	 *	Database details are defined here
+	 */
+	define("DB_HOST", "localhost");
+	define("DB_USER", "root");
+	define("DB_PASS", "root");
+	define("DB_NAME", "test");
+
+?>
