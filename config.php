41f8d41abe9778494f691683713f77669f03f42d
Keith McKiernan <keito-1@hotmail.com>
2014-02-24 11:53:58 +0000

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,13 @@
 <?php
 
-	/**
-	 *	Configuration: Database Connection 
-	 *	Database details are defined here
-	 */
-	define("DB_HOST", "localhost");
-	define("DB_USER", "root");
-	define("DB_PASS", "root");
-	define("DB_NAME", "test");
+/**
+ *  Configuration: Database Connection 
+ *  Database details are defined here
+ */
+	
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "test");
 
 ?>
